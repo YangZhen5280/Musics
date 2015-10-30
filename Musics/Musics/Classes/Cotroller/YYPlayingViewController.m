5599fb5e932c8afeaeae0ea2754f1a05127c016a
yangzhen <624526429@qq.com>
2015-10-30 22:23:24 +0800

    关闭播放器控制器操作

--- Musics/Musics/Classes/Cotroller/YYPlayingViewController.m
+++ Musics/Musics/Classes/Cotroller/YYPlayingViewController.m
@@ -18,16 +18,32 @@
     [super viewDidLoad];
 
 }
+#pragma mark - 对控制器的操作
 - (void)show {
-    
     UIWindow *window = [UIApplication sharedApplication].keyWindow;
+    window.userInteractionEnabled = NO;
+    
     self.view.frame = window.bounds;
     [window addSubview:self.view];
 
     self.view.y = self.view.height;
     [UIView animateWithDuration:1.0 animations:^{
-      
+
         self.view.y = 0;
+    } completion:^(BOOL finished) {
+        window.userInteractionEnabled = YES;
+    }];
+}
+- (IBAction)close {
+    UIWindow *window = [UIApplication sharedApplication].keyWindow;
+    window.userInteractionEnabled = NO;
+    
+    [UIView animateWithDuration:1.0 animations:^{
+
+        self.view.y = self.view.height;
+    } completion:^(BOOL finished) {
+        
+        window.userInteractionEnabled = YES;
     }];
 }
 
